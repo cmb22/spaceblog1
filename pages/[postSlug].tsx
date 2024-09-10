58ca7b5859dc539f10e67525f85c673706dd49bb
chris <chris@MacBook-Pro.local>
2024-09-10 17:31:03 +0200

    type errors

--- pages/[postSlug].tsx
+++ pages/[postSlug].tsx
@@ -7,40 +7,40 @@ import parse from "html-react-parser";
 import styles from "./index.module.css";
 
 export default function Post({ post, site, language, ...props }: any) {
-    // console.log("post", post)
-    const fullHead = post?.seo?.fullHead ? parse(post?.seo?.fullHead) : null;
-    return (
-        <div className={styles.container}>
-            {fullHead && <Head>{fullHead}</Head>}
-            <main className={styles.main}>
-                <h1 className={styles.title} style={{ "marginTop": "var(--wp--preset--spacing--10)" }}>{post.translation.title}</h1>
+  // console.log("post", post)
+  const fullHead = post?.seo?.fullHead ? parse(post?.seo?.fullHead) : null;
+  return (
+    <div className={styles.container}>
+      {fullHead && <Head>{fullHead}</Head>}
+      <main className={styles.main}>
+        <h1 className={styles.title} style={{ "marginTop": "var(--wp--preset--spacing--10)" }}>{post.translation.title}</h1>
 
-                <div className={styles.grid}>
-                    <div
-                        className={styles.content}
-                        dangerouslySetInnerHTML={{
-                            __html: post.translation.content,
-                        }}
-                    />
-                </div>
+        <div className={styles.grid}>
+          <div
+            className={styles.content}
+            dangerouslySetInnerHTML={{
+              __html: post.translation.content,
+            }}
+          />
+        </div>
 
-                {/* <p className={styles.backToHome}>
+        {/* <p className={styles.backToHome}>
           <Link href="/">
             <a className={styles.back}>&lt; Back To Home</a>
           </Link>
         </p> */}
-            </main>
-        </div>
-    );
+      </main>
+    </div>
+  );
 }
 
 export async function getStaticProps({ params, locale, ...props }: any) {
-    const { postSlug } = params;
-    const language = locale.toUpperCase();
-    const apolloClient = getApolloClient();
+  const { postSlug } = params;
+  const language = locale.toUpperCase();
+  const apolloClient = getApolloClient();
 
-    const data = await apolloClient.query({
-        query: gql`
+  const data = await apolloClient.query({
+    query: gql`
       query PostBySlug($slug: String!, $language: LanguageCodeEnum!) {
         generalSettings {
           title
@@ -73,51 +73,51 @@ export async function getStaticProps({ params, locale, ...props }: any) {
         }
       }
     `,
-        variables: {
-            slug: params.postSlug,
-            language,
-        },
-    });
-    const site = {
-        ...data?.data.generalSettings,
-    };
-    let post = data?.data.postBy;
-    if (post?.language?.slug && locale !== post.language.slug) {
-        return {
-            redirect: {
-                destination: `/${post.translation.slug}`
-            },
-            props: {
-                post,
-                language,
-                path: post?.translation?.slug ? `/${post.translation.slug}` : params.postSlug,
-                site,
-            },
-            revalidate: 10,
-        };
-    }
-
-
-
-
+    variables: {
+      slug: params.postSlug,
+      language,
+    },
+  });
+  const site = {
+    ...data?.data.generalSettings,
+  };
+  let post = data?.data.postBy;
+  if (post?.language?.slug && locale !== post.language.slug) {
     return {
-        props: {
-            post,
-            language,
-            path: post?.translation?.slug ? `/${post.translation.slug}` : params.postSlug,
-            site,
-        },
-        revalidate: 10,
+      redirect: {
+        destination: `/${post.translation.slug}`
+      },
+      props: {
+        post,
+        language,
+        path: post?.translation?.slug ? `/${post.translation.slug}` : params.postSlug,
+        site,
+      },
+      revalidate: 10,
     };
+  }
+
+
+
+
+  return {
+    props: {
+      post,
+      language,
+      path: post?.translation?.slug ? `/${post.translation.slug}` : params.postSlug,
+      site,
+    },
+    revalidate: 10,
+  };
 }
 
-export async function getStaticPaths({ locales, defaultLocale, ...props }) {
+export async function getStaticPaths({ locales, defaultLocale }: any) {
 
 
-    const apolloClient = getApolloClient();
+  const apolloClient = getApolloClient();
 
-    const data = await apolloClient.query({
-        query: gql`
+  const data = await apolloClient.query({
+    query: gql`
       {
         posts(first: 10000) {
           edges {
@@ -130,21 +130,21 @@ export async function getStaticPaths({ locales, defaultLocale, ...props }) {
         }
       }
     `,
-    });
+  });
 
-    const posts = data?.data.posts.edges.map(({ node }: any) => node);
+  const posts = data?.data.posts.edges.map(({ node }: any) => node);
 
-    const paths = posts.map(({ slug }: any) => {
-        return {
-            params: {
-                postSlug: slug,
-            },
-        };
-    });
-
-
+  const paths = posts.map(({ slug }: any) => {
     return {
-        paths: [],
-        fallback: "blocking",
+      params: {
+        postSlug: slug,
+      },
     };
+  });
+
+
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
 }
