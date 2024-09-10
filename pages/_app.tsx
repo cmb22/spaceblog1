0d666928e6e5832e976ceea0553f0b70c9bf6f00
chris <chris@MacBook-Pro.local>
2024-09-10 17:16:30 +0200

    header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,15 @@
 import "../globals.css";
-// import { NextUIProvider } from '@nextui-org/react'
-// import Header from "../components/Header";
+import { NextUIProvider } from '@nextui-org/react'
+import Header from "../components/Header";
 // import { GoogleAnalytics } from '@next/third-parties/google'
 
 function MyApp({ Component, pageProps }: any) {
     return (
         <>
-            {/* <NextUIProvider> */}
-            {/* <Header /> */}
-            <Component {...pageProps} />
-            {/* </NextUIProvider> */}
+            <NextUIProvider>
+                <Header />
+                <Component {...pageProps} />
+            </NextUIProvider>
         </>
     );
 }
